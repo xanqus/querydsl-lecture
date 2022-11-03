63fb62c0ec471dfd7ae962d8945bb1a22cffc566
xanqus <sbk98031816@gmail.com>
2022-11-03 15:32:59 +0900

    프로젝트 세팅 깃허브 연결

---
+++ src/main/java/com/lecture/querydsl_exam/QuerydslExamApplication.java
@@ -0,0 +1,13 @@
+package com.lecture.querydsl_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslExamApplication.class, args);
+	}
+
+}
