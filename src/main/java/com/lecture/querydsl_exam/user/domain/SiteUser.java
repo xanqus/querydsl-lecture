d9928ffd93930f2694c0544764e653399275db88
xanqus <sbk98031816@gmail.com>
2022-11-03 15:39:10 +0900

    compileQueryDsl 실행으로 엔티티로부터 Q클래스 생성

---
+++ src/main/java/com/lecture/querydsl_exam/user/domain/SiteUser.java
@@ -0,0 +1,25 @@
+package com.lecture.querydsl_exam.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+}
